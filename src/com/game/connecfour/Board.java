8f42ce20b287029692d31945c323ae09dd922f02
Javier <duranjavier@live.com>
2022-09-25 10:44:09 -0700

    Added fields, getters/setters, and ctor

--- src/com/game/connecfour/Board.java
+++ src/com/game/connecfour/Board.java
@@ -1,5 +1,50 @@
 package com.game.connecfour;
 
 class Board {
+    // Board Fields
+    private static int column;
+    private static int row;
+    private int lastColumn = 0;
+    private int lastTop = 0;
 
+    // Constructor
+    public Board(int column, int row) {
+        setColumn(column);
+        setRow(row);
+    }
+
+    // Methods
+
+    // Accessor Methods
+    public static int getColumn() {
+        return column;
+    }
+
+    public static void setColumn(int column) {
+        Board.column = column;
+    }
+
+    public static int getRow() {
+        return row;
+    }
+
+    public static void setRow(int row) {
+        Board.row = row;
+    }
+
+    public int getLastColumn() {
+        return lastColumn;
+    }
+
+    public void setLastColumn(int lastColumn) {
+        this.lastColumn = lastColumn;
+    }
+
+    public int getLastTop() {
+        return lastTop;
+    }
+
+    public void setLastTop(int lastTop) {
+        this.lastTop = lastTop;
+    }
 }
