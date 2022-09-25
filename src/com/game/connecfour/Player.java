7c2e01d36b0525ad7356721e5c58f47fa72bb396
Javier <duranjavier@live.com>
2022-09-25 10:59:12 -0700

    Added Player fields, getters/setters, and ctor, changes to Board

--- src/com/game/connecfour/Player.java
+++ src/com/game/connecfour/Player.java
@@ -1,5 +1,51 @@
 package com.game.connecfour;
 
 class Player {
+    // Fields
+    private String player1;
+    private String player2;
+    private int lastColumn = 0;
+    private int lastTop = 0;
 
+    // Constructor
+
+
+    public Player(String player1, String player2) {
+        setPlayer1(player1);
+        setPlayer2(player2);
+    }
+
+    // Accessor Methods
+
+    public String getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(String player1) {
+        this.player1 = player1;
+    }
+
+    public String getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(String player2) {
+        this.player2 = player2;
+    }
+
+    public int getLastColumn() {
+        return lastColumn;
+    }
+
+    public void setLastColumn(int lastColumn) {
+        this.lastColumn = lastColumn;
+    }
+
+    public int getLastTop() {
+        return lastTop;
+    }
+
+    public void setLastTop(int lastTop) {
+        this.lastTop = lastTop;
+    }
 }
