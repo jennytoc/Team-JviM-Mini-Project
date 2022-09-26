6686affe18fdaab9e0c02aa0a8a3bde6ece7a978
jennytoc <toctocanj@gmail.com>
2022-09-26 13:13:55 -0600

    Started forwardDiagonal

--- src/com/game/connecfour/Board.java
+++ src/com/game/connecfour/Board.java
@@ -1,17 +1,85 @@
 package com.game.connecfour;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Board {
     // Board Fields
+    private static final String[] PLAYERS = {"R", "Y"};
     private static int column;
     private static int row;
+    private String[][] board;
 
     // Constructor
     public Board(int column, int row) {
         setColumn(column);
         setRow(row);
+        initBoard();
     }
 
     // Methods
+    public void initBoard() {
+        board = new String[getRow()][getColumn()];
+        for (String[] row : board) {
+            Arrays.fill(row, "_|");
+        }
+        System.out.println(Arrays.deepToString(board));
+    }
+
+    // After this is called, it prints out the Board?
+    public boolean checkBoard() {
+        boolean result = false;
+        return result;
+    }
+
+    private boolean horizontalLine(String token) {
+        int streak = 0;
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < column; c++) {
+                if (board[r][c].equals(token)) {
+                    streak++;
+                    if (streak == 4) {
+                        return true;
+                    }
+                    else {
+                        streak = 0;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean verticalLine(String token) {
+        int streak = 0;
+        for (int c = 0; c < column; c++) {
+            for (int r = 0; r < row; r++) {
+                if (board[r][c].equals(token)) {
+                    streak++;
+                    if (streak == 4) {
+                        return true;
+                    }
+                    else {
+                        streak = 0;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean forwardDiagonal(String token) {
+        int streak = 0;
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < column; c++) {
+            }
+        }
+        return false;
+    }
+
+    private boolean backwardDiagonal(String token) {
+        return false;
+    }
 
     // Accessor Methods
     public static int getColumn() {
