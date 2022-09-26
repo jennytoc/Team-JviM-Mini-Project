1ac42cf62cbfff5c5a65222509aa009d9cecbf7b
jennytoc <toctocanj@gmail.com>
2022-09-26 10:18:22 -0600

    Added initBoard method, ConnectFourTest

--- src/com/game/connecfour/Board.java
+++ src/com/game/connecfour/Board.java
@@ -1,17 +1,37 @@
 package com.game.connecfour;
 
-class Board {
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Board {
     // Board Fields
     private static int column;
     private static int row;
+    private String[][] board;
 
     // Constructor
     public Board(int column, int row) {
         setColumn(column);
         setRow(row);
+        initBoard();
     }
 
     // Methods
+    public void initBoard() {
+        String[][] board = new String[getRow()][getColumn()];
+        for (String[] row : board) {
+            Arrays.fill(row, "_");
+        }
+    }
+
+    public boolean checkBoard() {
+        boolean result = false;
+        return result;
+    }
+
+    private void horizontalLine() {
+
+    }
 
     // Accessor Methods
     public static int getColumn() {
---
+++ src/com/game/connectfour/test/ConnectFourTest.java
@@ -0,0 +1,10 @@
+package com.game.connectfour.test;
+
+import com.game.connecfour.Board;
+
+class ConnectFourTest {
+
+    public static void main(String[] args) {
+        Board brd1 = new Board(7, 6);
+    }
+}
