9ff57857ba6fa8cee9f83f32ec875a705655a7c2
Javier <duranjavier@live.com>
2022-09-26 10:41:29 -0700

    Added Main() & Package, code for App

---
+++ src/com/game/connectfour/client/Main.java
@@ -0,0 +1,11 @@
+package com.game.connectfour.client;
+
+import com.game.connectfour.controller.ConnectFourApp;
+
+class Main {
+
+    public static void main(String[] args) {
+        ConnectFourApp app = new ConnectFourApp();
+        app.execute();
+    }
+}
