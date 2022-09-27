146bcb967092f9c065950a691941f86a7dd46828
Javier <duranjavier@live.com>
2022-09-27 08:52:56 -0700

    Finished App and modified dropToken()

--- src/com/game/connectfour/controller/ConnectFourApp.java
+++ src/com/game/connectfour/controller/ConnectFourApp.java
@@ -1,41 +1,44 @@
 package com.game.connectfour.controller;
 
 import com.game.connecfour.Board;
+import com.game.connecfour.Player;
 
 import java.util.Scanner;
 
 public class ConnectFourApp {
+    private static final char[] PLAYERS = {'R', 'Y'};
+    Player players = new Player("Joe", "Sally");
+
     public void execute() {
         welcome();
 
-        int row = 7;
-        int column = 8;
+        int row = 6;
+        int column = 7;
         int moves = row * column;
         Board board = new Board(column, row);
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please use 0-" + (column - 1) + " to choose a column.");
-        int input = scanner.nextInt();
+        System.out.println("Please use 1-" + column + " to choose a column.");
         System.out.println(board);
 
-        /*
+
         for (int player = 0; moves-- > 0; player = 1 - player) {
             char symbol = PLAYERS[player];
 
-            Player.dropToken(symbol, input);
+            players.dropToken(symbol, scanner);
 
             System.out.println(board);
 
-            if (Player.isWinning()) {
+            if (board.checkBoard(symbol, players.getLastTop(), players.getLastColumn())) {
                 System.out.println("\nPlayer " + symbol + " wins!");
                 return;
             }
         }
-        */
+
     }
 
     private void welcome() {
-        System.out.println("Welcome to Captain's Mistress");
+        System.out.println("Welcome to Connect4");
         System.out.println();
     }
 }
